56bf1c848e73c9a800edf63ba50b11eb9b517843
Thang Nguyen <thangnq2901@gmail.com>
2022-03-27 01:00:12 +0700

    fix socket connection

--- public/script.js
+++ public/script.js
@@ -12,141 +12,147 @@ $(document).ready(function () {
 	ballSpeed.hide();
 	readyButton.hide();
 
-	if (window.location.href.substring(7,8) == "l") {
-		connection = new WebSocket('ws://13.212.167.254:8080');
-		route = window.location.href.substring(22,27);
-	}
-	else if (window.location.href.substring(8,9) == "p") {
-		connection = new WebSocket('wss://png-game.herokuapp.com/');
-		route = window.location.href.substring(31,36);
-	}
-	else if (window.location.href.substring(7,8) == "p") {
-		connection = new WebSocket('ws://png-game.herokuapp.com/');
-		route = window.location.href.substring(30,35);
-	}
-	
-	var joystick = 
-		nipplejs.create({
-			zone: document.getElementById('joystickWrapper'),
-			color: 'white',
-			mode: 'semi'
-		});
-	
-	var canvas = document.getElementById('pongTable');
-	var ctx = canvas.getContext('2d');
-	var currentPlayer;
-	var keyUp = false;
-	var keyDown = false;
-	var player1Score = 0;
-	var player2Score = 0;
-	ctx.strokeStyle = 'white';
-	ctx.rect(375, 240, 10, 10);
-	ctx.rect(0, 225, 10, 50);
-	ctx.rect(740, 225, 10, 50);
-	ctx.stroke();
-	
-	readyButton.on('click', function() {
-		var ballSpeedVote = ballSpeed.val();
-		connection.send(route + ' ready ' + currentPlayer + ' ' + ballSpeedVote);
-		readyButton.hide();
-		ballSpeed.hide();
-		$('label').hide();
-	});
-	
-	$('#joinButton').on('click', function() {
-		connection.send(route + ' join');
-		$('#joinButton').hide();
-		readyButton.show();
-		ballSpeed.show();
-		$('label').show();
-	});
-	
-	// keyboard controls
-	window.onkeydown = function(e) {
-		if (e.keyCode === 38) {
-			keyUp = true;
-		}
-		if (e.keyCode === 40) {
-			keyDown = true;
-		}
-	}
-	
-	window.onkeyup = function(e) {
-		if (e.keyCode === 38) {
-			keyUp = false;
-		}
-		if (e.keyCode === 40) {
-			keyDown = false;
-		}
-	}
+  const host = window.location.hostname;
 
-	// mobile joystick controls
-	joystick.on('dir:up dir:down', function(evt, data) {
-		if (evt.type == 'dir:up') {
-			keyDown = false;
-			keyUp = true;
-		}
-		if (evt.type == 'dir:down') {
-			keyUp = false;
-			keyDown = true;
-		}
-	});
-	
-	joystick.on('start end', function(evt, data) {
-		if (evt.type == 'end') {
-			keyUp = false;
-			keyDown = false;
-		}
-	});
+  connection = new WebSocket(`ws://${host}:8080`);
+  route = window.location.pathname;
 
-	connection.onmessage = function (message) {
-		if (keyUp) {
-			connection.send(route + ' ' + currentPlayer + ' up');
-		}
-		else {
-			connection.send(route + ' ' + currentPlayer + ' up false');
-		}
-		if (keyDown) {
-			connection.send(route + ' ' + currentPlayer + ' down');
-		}
-		else {
-			connection.send(route + ' ' + currentPlayer + ' down false');
-		}
-		if (message.data.length === 1) {
-			// if message data length is 1 then we are receiving player join info
-			currentPlayer = message.data;
-		}
-		else if (message.data.substring(0, 5) === route && message.data.substring(6, 13) === "message") {
-			//if (message.data.substring(6, 13) === "message") {
-				writeMsg(message.data.substring(14, message.data.length));
-			//}
-		}
-		else if (message.data.substring(0, 1) == "{") {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			ctx.beginPath();
-			var gameObjs = JSON.parse(message.data);
-			if (gameObjs[route] != undefined) {
-				ctx.rect(gameObjs[route].puck.x, gameObjs[route].puck.y, gameObjs[route].puck.w, gameObjs[route].puck.h);
-				ctx.rect(gameObjs[route].leftpaddle.x, gameObjs[route].leftpaddle.y, gameObjs[route].leftpaddle.w, gameObjs[route].leftpaddle.h);
-				ctx.rect(gameObjs[route].rightpaddle.x, gameObjs[route].rightpaddle.y, gameObjs[route].rightpaddle.w, gameObjs[route].rightpaddle.h); 
-				ctx.stroke();
-				if (gameObjs[route].leftpaddle.score !== player1Score || gameObjs[route].rightpaddle.score !== player2Score) {
-					$('#player1Score').empty();
-					$('#player2Score').empty();
-					$('#player1Score').append(gameObjs[route].leftpaddle.score);
-					$('#player2Score').append(gameObjs[route].rightpaddle.score);
-					player1Score = gameObjs[route].leftpaddle.score;
-					player2Score = gameObjs[route].rightpaddle.score;
-				}
-				if (gameObjs[route].leftpaddle.score === 11) {
-					readyButton.show();
-				}
-				if (gameObjs[route].rightpaddle.score === 11) {
-					readyButton.show();
-				}
-			}
-		}
-	};
+  var joystick = nipplejs.create({
+    zone: document.getElementById('joystickWrapper'),
+    color: 'white',
+    mode: 'semi',
+  });
+
+  var canvas = document.getElementById('pongTable');
+  var ctx = canvas.getContext('2d');
+  var currentPlayer;
+  var keyUp = false;
+  var keyDown = false;
+  var player1Score = 0;
+  var player2Score = 0;
+  ctx.strokeStyle = 'white';
+  ctx.rect(375, 240, 10, 10);
+  ctx.rect(0, 225, 10, 50);
+  ctx.rect(740, 225, 10, 50);
+  ctx.stroke();
+
+  readyButton.on('click', function () {
+    var ballSpeedVote = ballSpeed.val();
+    connection.send(route + ' ready ' + currentPlayer + ' ' + ballSpeedVote);
+    readyButton.hide();
+    ballSpeed.hide();
+    $('label').hide();
+  });
+
+  $('#joinButton').on('click', function () {
+    connection.send(route + ' join');
+    $('#joinButton').hide();
+    readyButton.show();
+    ballSpeed.show();
+    $('label').show();
+  });
+
+  // keyboard controls
+  window.onkeydown = function (e) {
+    if (e.keyCode === 38) {
+      keyUp = true;
+    }
+    if (e.keyCode === 40) {
+      keyDown = true;
+    }
+  };
+
+  window.onkeyup = function (e) {
+    if (e.keyCode === 38) {
+      keyUp = false;
+    }
+    if (e.keyCode === 40) {
+      keyDown = false;
+    }
+  };
+
+  // mobile joystick controls
+  joystick.on('dir:up dir:down', function (evt, data) {
+    if (evt.type == 'dir:up') {
+      keyDown = false;
+      keyUp = true;
+    }
+    if (evt.type == 'dir:down') {
+      keyUp = false;
+      keyDown = true;
+    }
+  });
+
+  joystick.on('start end', function (evt, data) {
+    if (evt.type == 'end') {
+      keyUp = false;
+      keyDown = false;
+    }
+  });
+
+  connection.onmessage = function (message) {
+    if (keyUp) {
+      connection.send(route + ' ' + currentPlayer + ' up');
+    } else {
+      connection.send(route + ' ' + currentPlayer + ' up false');
+    }
+    if (keyDown) {
+      connection.send(route + ' ' + currentPlayer + ' down');
+    } else {
+      connection.send(route + ' ' + currentPlayer + ' down false');
+    }
+    if (message.data.length === 1) {
+      // if message data length is 1 then we are receiving player join info
+      currentPlayer = message.data;
+    } else if (message.data.substring(0, 5) === route && message.data.substring(6, 13) === 'message') {
+      //if (message.data.substring(6, 13) === "message") {
+      writeMsg(message.data.substring(14, message.data.length));
+      //}
+    } else if (message.data.substring(0, 1) == '{') {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      var gameObjs = JSON.parse(message.data);
+      if (gameObjs[route] != undefined) {
+        console.log(gameObjs[route].puck);
+        ctx.rect(gameObjs[route].puck.x, gameObjs[route].puck.y, gameObjs[route].puck.w, gameObjs[route].puck.h);
+        if (gameObjs[route].puck.y <= 0 || gameObjs[route].puck.y >= 490) {
+          var audio = new Audio('/audio/Glass_Error1.wav');
+          audio.play();
+        }
+        if (gameObjs[route].puck.x === 100) {
+          var audio = new Audio('/audio/Glass_Done5.wav');
+          audio.play();
+        }
+        ctx.rect(
+          gameObjs[route].leftpaddle.x,
+          gameObjs[route].leftpaddle.y,
+          gameObjs[route].leftpaddle.w,
+          gameObjs[route].leftpaddle.h
+        );
+        ctx.rect(
+          gameObjs[route].rightpaddle.x,
+          gameObjs[route].rightpaddle.y,
+          gameObjs[route].rightpaddle.w,
+          gameObjs[route].rightpaddle.h
+        );
+        ctx.stroke();
+        if (gameObjs[route].leftpaddle.score !== player1Score || gameObjs[route].rightpaddle.score !== player2Score) {
+          $('#player1Score').empty();
+          $('#player2Score').empty();
+          $('#player1Score').append(gameObjs[route].leftpaddle.score);
+          $('#player2Score').append(gameObjs[route].rightpaddle.score);
+          player1Score = gameObjs[route].leftpaddle.score;
+          player2Score = gameObjs[route].rightpaddle.score;
+        }
+        if (gameObjs[route].leftpaddle.score === 11) {
+          readyButton.show();
+        }
+        if (gameObjs[route].rightpaddle.score === 11) {
+          readyButton.show();
+        }
+      }
+    }
+  };
 	
 	msgModal.dialog({
 		autoOpen: false,
