af76b38628738ade4366b8080252342697ff102a
Le Thanh Long <long@creativeforce.io>
2022-03-27 11:53:01 +0700

    Add copy link

--- public/home.js
+++ public/home.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+	const host = window.location.host;
+	
 	var totalGames = 0;
 	var totalPlayers = 0;
 	var route = "";
@@ -9,7 +12,6 @@ $(document).ready(function () {
         route += possible.charAt(Math.floor(Math.random() * possible.length));
       }
     }
-	
 	
 	function populateGameList() {
 		$.ajax({
@@ -22,6 +24,7 @@ $(document).ready(function () {
 						<p class="col-1">Game ID</p>
 						<p class="col-2">Players</p>
 						<p class="col-3">Join / Spectate</p>
+						<p class="col-3">Link Invite</p>
 					</div>`);
         $('#games').append(`<div class="table-body">`);
         Object.keys(data).forEach(game => {
@@ -37,6 +40,7 @@ $(document).ready(function () {
 							<p class="col-1">${game}</p>
 							<p class="col-2">(${data[game].players}/2)</p>
 							<p class="col-3"><button class="btn btn-style-2 gameJoin" data-index=${game}>${joinSpectate}</button></<td>
+							<p class="col-3"><button class="btn btn-style-2 linkJoin" data-index=${game}>Copy Link</button></<td>
 						</tr>`);
         });
         // close table body
@@ -51,6 +55,27 @@ $(document).ready(function () {
 						var gameRoute = $(this).data('index');
 						window.location.href = '/' + gameRoute;
 						//console.log(gameNum);
+					});
+				});
+
+				$('.linkJoin').each(function() {
+					$(this).on("click", function(event){
+						event.preventDefault();
+						var gameRoute = $(this).data('index');
+						
+						var text = `${host}://${gameRoute}`;
+
+						  const textInput = document.createElement('textarea');
+						  textInput.innerHTML = text;
+						  
+						var	parentEl = document.body;
+						  
+						  parentEl.appendChild(textInput);
+						  textInput.select();
+						  document.execCommand('copy');
+						  parentEl.removeChild(textInput);
+
+						  $(this).text('Copied');
 					});
 				});
 			}
