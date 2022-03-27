fc299d17b08cb1d80de337a9daa21e42875c2d39
Thang Nguyen <thangnq2901@gmail.com>
2022-03-27 10:09:45 +0700

    update ui

--- public/home.js
+++ public/home.js
@@ -18,32 +18,32 @@ $(document).ready(function () {
 			success: function(data) {
 				$('#games').empty();
 				$('#games').append(`
-					<tr>
-						<th>Game ID</th>
-						<th>Players</th>
-						<th>Join / Spectate</th>
-					</tr>`);
-				Object.keys(data).forEach(game => {
-					var joinSpectate;
-					totalGames++;
-					totalPlayers += data[game].players;
-					
-					if (data[game].players > 1)
-						joinSpectate = "Spectate";
-					else
-						joinSpectate = "Join";
-					
-					$('#games').append(`
-						<tr>
-							<td>${game}</td>
-							<td>(${data[game].players}/2)</td>
-							<td><button class="gameJoin" data-index=${game}>${joinSpectate}</button></<td>
+					<div class="table-head">
+						<p class="col-1">Game ID</p>
+						<p class="col-2">Players</p>
+						<p class="col-3">Join / Spectate</p>
+					</div>`);
+        $('#games').append(`<div class="table-body">`);
+        Object.keys(data).forEach(game => {
+          var joinSpectate;
+          totalGames++;
+          totalPlayers += data[game].players;
+
+          if (data[game].players > 1) joinSpectate = 'Spectate';
+          else joinSpectate = 'Join';
+
+          $('#games').append(`
+						<div class="table-tr">
+							<p class="col-1">${game}</p>
+							<p class="col-2">(${data[game].players}/2)</p>
+							<p class="col-3"><button class="btn btn-style-2" data-index=${game}>${joinSpectate}</button></<td>
 						</tr>`);
-				});
-				
+        });
+        // close table body
+        $('#games').append(`</div>`);
 				$('#status').empty();
-				$('#status').append(`Total number of games: ${totalGames} <br>
-									Players currently playing: ${totalPlayers}`);
+				// $('#status').append(`Total number of games: ${totalGames} <br>
+				// 					Players currently playing: ${totalPlayers}`);
 				
 				$('.gameJoin').each(function() {
 					$(this).on("click", function(event){
